7c3d449dfd0f028d556e57c4524bc3071e8e524a
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-10-31 16:01:33 +0900

    New: Constance.class.php

---
+++ Constance.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * unit-shortenurl:/Constance.class.php
+ *
+ * @created   2019-10-31
+ * @version   1.0
+ * @package   unit-shortenurl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\SHORTENURL;
+
+/** Used class
+ *
+ */
+use OP\OP_CORE;
+
+/** Constance
+ *
+ * @created   2019-10-31
+ * @version   1.0
+ * @package   unit-shortenurl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Constance
+{
+	/** Used trait
+	 *
+	 */
+	use OP_CORE;
+
+	//	...
+	const _TABLE_ = 't_shortenurl';
+}
