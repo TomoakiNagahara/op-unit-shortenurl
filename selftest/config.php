da4e73d8eedfa8df9d1a33a12bb9fd0102a924be
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-10-31 17:46:35 +0900

    New: selftest/config.php

---
+++ selftest/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * unit-shortenurl:/selftest/config.php
+ *
+ * @creation  2019-10-31
+ * @version   1.0
+ * @package   unit-shortenurl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
