9bf3e8f0ee4c4eb181b3b755ad226e373fcf73d7
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-10-31 17:46:19 +0900

    New: Selftest.class.php

---
+++ Selftest.class.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * unit-shortenurl:/Selftest.class.php
+ *
+ * @created   2019-10-31
+ * @version   1.0
+ * @package   unit-shortenurl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\SHORTENURL;
+
+/** Used class
+ *
+ */
+use OP\OP_CORE;
+use OP\OP_UNIT;
+
+/** Selftest
+ *
+ * @created   2019-10-31
+ * @version   1.0
+ * @package   unit-shortenurl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Selftest
+{
+	/** trait
+	 *
+	 */
+	use OP_CORE, OP_UNIT;
+
+	/** Automatically
+	 *
+	 * @created
+	 */
+	function Auto()
+	{
+
+	}
+}
