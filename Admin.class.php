0b08ef7aec6ed995db9898b969d3a8676a6ad330
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-10-31 16:02:22 +0900

    New: Admin.class.php

---
+++ Admin.class.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * unit-shortenurl:/Admin.class.php
+ *
+ * @created   2019-10-31
+ * @version   1.0
+ * @package   unit-shortenurl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/** namespace
+ *
+ */
+namespace OP\UNIT\SHORTENURL;
+
+/** Used class
+ *
+ */
+use OP\OP_CORE;
+use OP\OP_UNIT;
+
+/** Admin
+ *
+ * @created   2019-10-31
+ * @version   1.0
+ * @package   unit-shortenurl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+class Admin
+{
+	/** trait
+	 *
+	 */
+	use OP_CORE, OP_UNIT;
+
+	/** Automatically
+	 *
+	 */
+	function Auto()
+	{
+
+	}
+
+	/** Admin
+	 *
+	 */
+	function Admin()
+	{
+		include(__DIR__.'/Admin.class.php');
+		return new Admin();
+	}
+}
