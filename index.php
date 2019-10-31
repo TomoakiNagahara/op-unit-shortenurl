8619b2e66104b86f6e39a93cde4692842129b565
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-10-31 16:00:02 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * unit-shortenurl:/index.php
+ *
+ * @creation  2019-10-31
+ * @version   1.0
+ * @package   unit-shortenurl
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include_once(__DIR__.'/Common.class.php');
+include_once(__DIR__.'/Constance.class.php');
+include_once(__DIR__.'/ShortenURL.class.php');
